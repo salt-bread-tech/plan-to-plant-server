738b26c59f479ab6b67a17f2104157d5f1b35042
Better Jeong <eunjeong42na@gmail.com>
2023-03-16 21:31:44 +0900

    스프링부트 프로젝트

---
+++ src/main/java/beom/plantoplantserver/PlanToPlantServerApplication.java
@@ -0,0 +1,13 @@
+package beom.plantoplantserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanToPlantServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanToPlantServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/beom/plantoplantserver/PlanToPlantServerApplicationTests.java
@@ -0,0 +1,13 @@
+package beom.plantoplantserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanToPlantServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
