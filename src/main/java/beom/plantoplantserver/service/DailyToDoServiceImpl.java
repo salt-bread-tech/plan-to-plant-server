fc4ef7855e16dd7a8c9c6ca368824f81403244cb
GiCheols <ngc4665@naver.com>
2023-04-26 22:31:39 +0900

    Update DailyToDoController

--- src/main/java/beom/plantoplantserver/service/DailyToDoServiceImpl.java
+++ src/main/java/beom/plantoplantserver/service/DailyToDoServiceImpl.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,14 +17,31 @@ public class DailyToDoServiceImpl implements DailyToDoService{
     private final CalendarRepo calendarRepo;
 
     @Override
-    public List<Calendar> getToDoForToday() {
+    public List<Calendar> getToDoForToday(String user_id) {
         LocalDate now = LocalDate.now();
-        return calendarRepo.findByDate(now);
+
+        return getUserDailyToDo(user_id, now);
     }
 
     @Override
     public List<Calendar> getToDoForDate(CalendarRequest request) {
         LocalDate date = LocalDate.of(request.getYear(), request.getMonth(), request.getDay());
-        return calendarRepo.findByDate(date);
+        String uId = request.getUser_id();
+
+        return getUserDailyToDo(uId, date);
+    }
+
+    private List<Calendar> getUserDailyToDo(String user_id, LocalDate now){
+        List<Calendar> calendars = calendarRepo.findByUserIdAndDate(user_id, now);
+        List<Calendar> resCalendars = new ArrayList<>();
+        for(Calendar c : calendars){
+            resCalendars.add(Calendar.builder()
+                    .id(c.getId())
+                    .date(c.getDate())
+                    .toDo(c.getToDo())
+                    .toDoCompleted(c.getToDoCompleted())
+                    .build());
+        }
+        return resCalendars;
     }
 }
