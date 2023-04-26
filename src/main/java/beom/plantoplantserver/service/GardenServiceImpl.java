158ad38cc2618a8faf7d58f76f62876b09eccdcd
GiCheols <ngc4665@naver.com>
2023-04-26 22:54:16 +0900

    Update GardenServiceImpl.java

--- src/main/java/beom/plantoplantserver/service/GardenServiceImpl.java
+++ src/main/java/beom/plantoplantserver/service/GardenServiceImpl.java
@@ -5,6 +5,7 @@ import beom.plantoplantserver.repository.GardenRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,8 +17,21 @@ public class GardenServiceImpl implements GardenService{
 
     @Override
     public List<Garden> getPlantByUserId(String user_id) {
-        List<Garden> gardens = gardenRepo.findByUserId(user_id);
+        return getPlantList(user_id);
+    }
 
-        return gardenRepo.findByUserId(user_id);
+    private List<Garden> getPlantList(String user_id){
+        List<Garden> gardens = gardenRepo.findByUserId(user_id);
+        List<Garden> resGardens = new ArrayList<>();
+
+        for(Garden g : gardens){
+            resGardens.add(Garden.builder()
+                    .id(g.getId())
+                    .plant(g.getPlant())
+                    .count(g.getCount())
+                    .isFound(g.isFound())
+                    .build());
+        }
+        return resGardens;
     }
 }
